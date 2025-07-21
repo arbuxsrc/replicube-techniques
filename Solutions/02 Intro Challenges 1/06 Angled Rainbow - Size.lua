b7aa60a17ced545192022755157a1946d385ee49
Roger Bucks <Roger.Bucks@dominos.com.au>
2025-07-21 22:34:01 +1000

    Angled Rainbow solution

---
+++ Solutions/02 Intro Challenges 1/06 Angled Rainbow - Size.lua
@@ -0,0 +1,8 @@
+--[[
+    Author: Roger
+    Created: 2025-07-21
+    Description: 
+    Size: 10
+    Cycles per Voxel: 4.286
+]]
+return z==0 and x+y+10
---
+++ Solutions/02 Intro Challenges 1/06 Angled Rainbow - Speed.lua
@@ -0,0 +1,10 @@
+--[[
+    Author: Roger
+    Created: 2025-07-21
+    Description: 
+    Size: 12
+    Cycles per Voxel: 3.286
+]]
+if z==0 then
+	return x+y+10
+end
