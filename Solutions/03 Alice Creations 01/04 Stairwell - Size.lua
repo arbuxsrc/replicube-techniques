97f778cecce1336a3339d130d2dd00077b67bf5a
Roger Bucks <Roger.Bucks@dominos.com.au>
2025-07-21 23:15:45 +1000

    Stairwell Solutions

---
+++ Solutions/03 Alice Creations 01/04 Stairwell - Size.lua
@@ -0,0 +1,10 @@
+--[[
+    Author: Roger
+    Created: 2025-07-21
+    Description: 
+    Size: 16
+    Cycles per Voxel: 11.901
+]]
+
+return	z==0 and RED or
+		sign(z)*x==y and GREY
---
+++ Solutions/03 Alice Creations 01/04 Stairwell - Speed.lua
@@ -0,0 +1,11 @@
+--[[
+    Author: Roger
+    Created: 2025-07-21
+    Description: 
+    Size: 33
+    Cycles per Voxel: 5.314
+]]
+
+if z==0 then return RED end
+if z<0 and -x==y then return GREY end
+if z>0 and x==y then return GREY end
