f353599640c30cd9f9830e271f01a3b8995b9c3f
Roger Bucks <Roger.Bucks@dominos.com.au>
2025-07-21 22:18:18 +1000

    Diagonal Line solutions

---
+++ Solutions/02 Intro Challenges 1/02 Diagonal Line - Size.lua
@@ -0,0 +1,8 @@
+--[[
+    Author: Roger
+    Created: 2025-07-21
+    Description: 
+    Size: 10
+    Cycles per Voxel: 4.163
+]]
+return x==y and y==z and GREEN
---
+++ Solutions/02 Intro Challenges 1/02 Diagonal Line - Speed.lua
@@ -0,0 +1,8 @@
+--[[
+    Author: Roger
+    Created: 2025-07-21
+    Description: 
+    Size: 12
+    Cycles per Voxel: 3.163
+]]
+if x==y and y==z then return GREEN end
