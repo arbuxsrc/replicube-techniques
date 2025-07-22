dc70b06251defe079bd5e75c368308d91eb231da
Roger Bucks <Roger.Bucks@dominos.com.au>
2025-07-22 21:38:20 +1000

    Little Chair block simplification

--- Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
+++ Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
@@ -9,5 +9,4 @@ return  (
 		y>=0 and z>=0 or
 		y<=-2 and z==0 or
 		x==0 and (y<=-2 or y==0 or y==1)
-	) and 0 or
-	y<=2 and max(abs(x),abs(z))<=1 and ORANGE
+	) and 0 or x^2+z^2<=2 and y<3 and ORANGE
