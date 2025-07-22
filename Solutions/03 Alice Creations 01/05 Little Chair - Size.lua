b2dfb0cf13dbae3a1a22cb55d33a5357f1b23f8a
Roger Bucks <Roger.Bucks@dominos.com.au>
2025-07-23 00:33:34 +1000

    Alternate approach - simplify from side view and then remove centre holes front front

--- Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
+++ Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
@@ -2,11 +2,14 @@
     Author: Roger
     Created: 2025-07-21
     Description: This approach creates a solid orange block but removes portions of the block
-    Size: 45
-    Cycles per Voxel: 8.816
+    Size: 44
+    Cycles per Voxel: 8.571
 ]]
 
-return not (
-		y<=-2 and z*x==0 or
-		y>=0 and (z>=0 or x==0 and y<2)
-	) and x^2+z^2 <= 2 and y < 3 and ORANGE
+return -- looking at the chair from the side facing to the right
+	abs(x)<=1 and
+	(z==-1 and y < 3 or
+	z==0 and y == -1 or
+	z==1 and y <= -1) and
+	-- remove the holes in the middle section facing from the front
+	(x~=0 or y%3==2) and ORANGE
