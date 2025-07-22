6754822f29d4510ded2d326fc0ef2c66ddcfa8be
Roger Bucks <Roger.Bucks@dominos.com.au>
2025-07-22 21:44:32 +1000

    Little Chair - x*y=0 same as (x==0 or y==0)

--- Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
+++ Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
@@ -2,11 +2,11 @@
     Author: Roger
     Created: 2025-07-21
     Description: This approach creates a solid orange block but removes portions of the block
-    Size: 52
-    Cycles per Voxel: 9.245
+    Size: 48
+    Cycles per Voxel: 9.417
 ]]
-return  (
+return  not (
 		y>=0 and z>=0 or
-		y<=-2 and z==0 or
-		x==0 and (y<=-2 or y==0 or y==1)
-	) and 0 or x^2+z^2<=2 and y<3 and ORANGE
+		y<=-2 and z*x==0 or
+		x==0 and inrange(y,0,1)
+	) and x^2+z^2<=2 and y<3 and ORANGE
