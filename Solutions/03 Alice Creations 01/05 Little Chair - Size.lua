5903a49e3a8e840f5b255880e9e790b22df69ca2
Roger Bucks <Roger.Bucks@dominos.com.au>
2025-07-22 21:52:56 +1000

    Further optimisation of the subtractive option

--- Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
+++ Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
@@ -2,11 +2,10 @@
     Author: Roger
     Created: 2025-07-21
     Description: This approach creates a solid orange block but removes portions of the block
-    Size: 48
-    Cycles per Voxel: 9.417
+    Size: 46
+    Cycles per Voxel: 8.816
 ]]
-return  not (
-		y>=0 and z>=0 or
+return not (
 		y<=-2 and z*x==0 or
-		x==0 and inrange(y,0,1)
-	) and x^2+z^2<=2 and y<3 and ORANGE
+		y>=0 and (z>=0 or (x==0 and y<2))
+	) and x^2+z^2 <= 2 and y < 3 and ORANGE
