bb27700c9a380dee5787749b5354d46cb686722d
Roger Bucks <Roger.Bucks@dominos.com.au>
2025-07-23 00:39:20 +1000

    Finding formulae to match the pattern of little chair

--- Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
+++ Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
@@ -8,8 +8,7 @@
 
 return -- looking at the chair from the side facing to the right
 	abs(x)<=1 and
-	(z==-1 and y < 3 or
-	z==0 and y == -1 or
-	z==1 and y <= -1) and
+	(abs(z)==1 and y <= 1-z*1.5 or
+	z==0 and y == -1) and
 	-- remove the holes in the middle section facing from the front
 	(x~=0 or y%3==2) and ORANGE
