7542b448852d4e2158bacd5f24ee3793a0e711e9
Roger Bucks <Roger.Bucks@dominos.com.au>
2025-07-23 00:19:41 +1000

    Latest solution for little chair

--- Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
+++ Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
@@ -2,10 +2,11 @@
     Author: Roger
     Created: 2025-07-21
     Description: This approach creates a solid orange block but removes portions of the block
-    Size: 46
+    Size: 45
     Cycles per Voxel: 8.816
 ]]
+
 return not (
 		y<=-2 and z*x==0 or
-		y>=0 and (z>=0 or (x==0 and y<2))
+		y>=0 and (z>=0 or x==0 and y<2)
 	) and x^2+z^2 <= 2 and y < 3 and ORANGE
