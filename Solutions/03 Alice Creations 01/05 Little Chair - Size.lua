a502f9b8fada5279a168f06e6aab7aba9b1c73ff
Roger Bucks <Roger.Bucks@dominos.com.au>
2025-07-22 21:05:13 +1000

    Changed Tiny Chair solution to be subtractive from an orange block

--- Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
+++ Solutions/03 Alice Creations 01/05 Little Chair - Size.lua
@@ -1,11 +1,11 @@
 --[[
     Author: Roger
     Created: 2025-07-21
-    Description: 
-    Size: 70
-    Cycles per Voxel: 8.778
+    Description: This approach creates a solid orange block but removes portions of the block
+    Size: 57
+    Cycles per Voxel: 12.096
 ]]
-return --y==3 and 0 or -- top row has no cells
-		(z == -1 and y<3 and (x ==-1 or x==1 or y == 2 and x==0) or -- Vertical back legs, remainder of top back bar
-		y == -1 and x >=-1 and x<=1 and z>=-1 and z<=1 or -- seat
-		y < -1 and (x ==-1 or x==1) and z==1) and ORANGE -- front legs
+return  y>=0 and z>=0 and 0 or
+	y<=-2 and z==0 and 0 or
+	x==0 and (y<=-2 or y==0 or y==1) and 0 or
+	y<=2 and max(abs(x),abs(z))<=1 and ORANGE
