6cfbd5f4dd442414530c7badbf7925a6c1367b7f
Roger Bucks <Roger.Bucks@dominos.com.au>
2025-07-24 21:39:07 +1000

    Reduced Doggo size

--- Solutions/04 Alice Creations 02/01 Doggo - Size.lua
+++ Solutions/04 Alice Creations 02/01 Doggo - Size.lua
@@ -2,26 +2,23 @@
     Author: Roger
     Created: 2025-07-24
     Description: 
-    Size: 153
-    Cycles per Voxel: 16.093
+    Size: 148
+    Cycles per Voxel: 20.073
 ]]
 
-return 
-  abs(x)==1 and (
-    y==-3 and (z==2 or z==-3) and ORANGE
-    or y==-2 and (z==2 or z==-3) and BROWN
-    or inrange(y,-1,0) and z<3 and BROWN
-    or y==1 and z==3 and ORANGE
-    or inrange(y,1,2) and inrange(z,1,2) and BROWN
-    or y==3 and z==1 and BROWN
-  ) or x==0 and (
-    inrange(y,-1,0) and z<3 and BROWN
-    or (y==1 and (
-      z==1 and BROWN
-      or (z>=2 or z==-3) and ORANGE
-    ))
-    or y==2 and (
-      (z==2 or z<=-2) and ORANGE
-      or z==1 and BROWN
-    )
-  )
+return abs(x)>1 and 0
+	or   x~=0 and (
+					   y==-3 and (z==-3 or z==2) and ORANGE
+					or y==-2 and (z==-3 or z==2) and BROWN
+					or y==3 and z==1 and BROWN
+					or inrange(y,1,2) and inrange(z,1,2) and BROWN
+					)
+	or   x==0 and (
+		z==-3 and inrange(y,1,2) and ORANGE or
+		z==-2 and y==2 and ORANGE
+	)
+	or   inrange(y,-1,0) and z < 3 and BROWN
+	or   inrange(y,1,2) and z==1 and BROWN
+	or	 y==1 and z>=2 and ORANGE
+	or   y==2 and z==2 and ORANGE
+	
